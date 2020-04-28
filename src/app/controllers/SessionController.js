538d6245e26924b57e4194b4475b1b1a670afdbe
Francisco Marcos <francisco-marcos@hotmail.com>
2020-04-27 23:35:52 -0300

    Subindo arquivos

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,23 @@
+const { User } = require('../models')
+
+class SesssionController {
+	async store(req, res) {
+		const { email, password } = req.body
+		const user = await User.findOne({ where: { email } })
+
+		if (!user) {
+			return res.status(401).json({ message: 'User not found' })
+		}
+
+		if (!(await user.checkPassword(password))) {
+			return res.status(401).json({ message: 'Password Incorrect' })
+		}
+
+		return res.json({
+			user,
+			token: user.generateToken(),
+		})
+	}
+}
+
+module.exports = new SesssionController()
